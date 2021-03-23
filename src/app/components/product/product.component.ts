b66bcc026bf66b4cdff3efd384a0cc4433cdffef
Gamze <gamzeburcinaydin@gmail.com>
2021-03-23 17:05:58 +0300

    commit

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
-import { ProductResponseModel } from 'src/app/models/productResponseModel';
 import { ProductService } from 'src/app/services/product.service';
 
 //axios, 
@@ -15,10 +15,16 @@ export class ProductComponent implements OnInit {
  dataLoaded=false;
  
  
-  constructor(private productService:ProductService ) { }
+  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.getProducts();
+    this.activatedRoute.params.subscribe(params=>{
+      if(params["categoryId"]){
+        this.getProductsByCategory(params["categoryId"])
+      }else{
+        this.getProducts()
+      }
+    })
   }
   
   getProducts(){
@@ -27,5 +33,11 @@ export class ProductComponent implements OnInit {
        this.dataLoaded=true;
      })
   }
+  getProductsByCategory(categoryId:number){
+    this.productService.getProductsByCategory(categoryId).subscribe(response=>{
+      this.products=response.data
+      this.dataLoaded=true;
+    })
+ }
 
 }
